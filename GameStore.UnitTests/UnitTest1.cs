825032205c81d70292cbea7aa4b14e6af0f77ed1
kassymov17 <kassymov17@gmail.com>
2016-08-28 17:52:07 +0600

    added pagination class,html helper

--- GameStore.UnitTests/UnitTest1.cs
+++ GameStore.UnitTests/UnitTest1.cs
@@ -3,68 +3,98 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+using GameStore.WebUI.Controllers;
+using Moq;
+using GameStore.WebUI.HtmlHelpers;
+using GameStore.WebUI.Models;
+using System.Web.Mvc;
 
 namespace GameStore.UnitTests
 {
-    /// <summary>
-    /// Сводное описание для UnitTest1
-    /// </summary>
     [TestClass]
     public class UnitTest1
     {
-        public UnitTest1()
+        [TestMethod]
+        public void Can_Paginate()
         {
-            //
-            // TODO: добавьте здесь логику конструктора
-            //
-        }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Получает или устанавливает контекст теста, в котором предоставляются
-        ///сведения о текущем тестовом запуске и обеспечивается его функциональность.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
+            //организация-arrange
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
             {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
+                new Game{GameId=1,Name="Игра1"},
+                new Game{GameId=2,Name="Игра2"},
+                new Game{GameId=3,Name="Игра3"},
+                new Game{GameId=4,Name="Игра4"},
+                new Game{GameId=5,Name="Игра5"}
+            });
+            GameController controller = new GameController(mock.Object);
+            controller.pageSize = 3;
 
-        #region Дополнительные атрибуты тестирования
-        //
-        // При написании тестов можно использовать следующие дополнительные атрибуты:
-        //
-        // ClassInitialize используется для выполнения кода до запуска первого теста в классе
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // ClassCleanup используется для выполнения кода после завершения работы всех тестов в классе
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // TestInitialize используется для выполнения кода перед запуском каждого теста 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // TestCleanup используется для выполнения кода после завершения каждого теста
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
+
+            //Действие(act)
+            GamesListViewModel result = (GamesListViewModel)controller.List(2).Model;
+
+            //утверждение(assert)
+            List<Game> games = result.Games.ToList();
+            Assert.IsTrue(games.Count==2);
+            Assert.AreEqual(games[0].Name, "Игра4");
+            Assert.AreEqual(games[1].Name, "Игра5");
+        }
 
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Generate_Page_Links()
         {
-            //
-            // TODO: добавьте здесь логику теста
-            //
+            //организация
+            HtmlHelper myHelper = null;
+            
+            //организация создание объекта pagingInfo
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage=3,
+                TotalItems=28,
+                ItemsPerPage=10
+            };
+
+            //организация-настройка делегата с помощью лямбда-выражения
+            Func<int, string> pageUrlDelegate = i => "Page" + i;
+
+            //действие
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo,pageUrlDelegate);
+
+            //утверждение
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>"
+                +@"<a class=""btn btn-default"" href=""Page2"">2</a>"
+                + @"<a class=""btn btn-default btn-primary selected"" href=""Page3"">3</a>",
+                result.ToString());
+        }
+
+        [TestMethod]
+        public void Can_Send_Pagination_View_Model()
+        {
+            //организация
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game{GameId=1,Name="Игра1"},
+                new Game{GameId=2,Name="Игра2"},
+                new Game{GameId=3,Name="Игра3"},
+                new Game{GameId=4,Name="Игра4"},
+                new Game{GameId=5,Name="Игра5"}
+            });
+            GameController controller = new GameController(mock.Object);
+            controller.pageSize = 3;
+
+            //act
+            GamesListViewModel result = (GamesListViewModel)controller.List(2).Model;
+
+            //Assert
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems,5);
+            Assert.AreEqual(pageInfo.TotalPages, 2);
         }
     }
 }
---
+++ GameStore.WebUI/Models/PagingInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameStore.WebUI.Models
+{
+    public class PagingInfo
+    {
+        //кол-во товаров
+        public int TotalItems { get; set; }
+
+        //кол-во товаров на одной странице
+        public int ItemsPerPage { get; set; }
+
+        //номер текущей страницы
+        public int CurrentPage { get; set; }
+
+        //общее количество страниц
+        public int TotalPages
+        {
+            get { return (int)Math.Ceiling((decimal)TotalItems/ItemsPerPage); }
+        }
+    }
+}
